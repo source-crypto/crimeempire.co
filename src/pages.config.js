5c2ecacaf4660825926de973232c9c36c9a40f0d
Givonni Richardson <cat177357@gmail.com>
2026-01-12 14:20:27 +0000

    Noir Case Management System

--- src/pages.config.js
+++ src/pages.config.js
@@ -1,29 +1,35 @@
-import Dashboard from './pages/Dashboard';
-import Crew from './pages/Crew';
-import Territories from './pages/Territories';
-import Heists from './pages/Heists';
-import Garage from './pages/Garage';
 import Auction from './pages/Auction';
+import Crew from './pages/Crew';
+import Dashboard from './pages/Dashboard';
 import Enterprises from './pages/Enterprises';
-import Settings from './pages/Settings';
-import Metaverse from './pages/Metaverse';
+import Garage from './pages/Garage';
 import Governance from './pages/Governance';
+import Heists from './pages/Heists';
+import Home from './pages/Home';
+import Metaverse from './pages/Metaverse';
 import PlayerSetup from './pages/PlayerSetup';
+import Settings from './pages/Settings';
+import Territories from './pages/Territories';
+import Cases from './pages/Cases';
+import CaseDetail from './pages/CaseDetail';
 import __Layout from './Layout.jsx';
 
 
 export const PAGES = {
-    "Dashboard": Dashboard,
-    "Crew": Crew,
-    "Territories": Territories,
-    "Heists": Heists,
-    "Garage": Garage,
     "Auction": Auction,
+    "Crew": Crew,
+    "Dashboard": Dashboard,
     "Enterprises": Enterprises,
-    "Settings": Settings,
-    "Metaverse": Metaverse,
+    "Garage": Garage,
     "Governance": Governance,
+    "Heists": Heists,
+    "Home": Home,
+    "Metaverse": Metaverse,
     "PlayerSetup": PlayerSetup,
+    "Settings": Settings,
+    "Territories": Territories,
+    "Cases": Cases,
+    "CaseDetail": CaseDetail,
 }
 
 export const pagesConfig = {
